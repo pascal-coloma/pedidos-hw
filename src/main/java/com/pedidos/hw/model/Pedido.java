498cc7600ea435a9c3ad91ea8bd97366b0b0863c
Pascal Coloma <coloma.pascal@gmail.com>
2025-05-14 11:26:54 -0400

    importe de todo en jakarta persistence para limpieza del codigo

--- src/main/java/com/pedidos/hw/model/Pedido.java
+++ src/main/java/com/pedidos/hw/model/Pedido.java
@@ -5,12 +5,7 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
